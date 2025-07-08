d7b7dc109b4021e898614d16fdc2a61824d1f2ca
1uminou3 <weiqicai5@gmail.com>
2025-07-08 12:59:49 +0800

    Update consumption.js

--- Website/consumption/consumption.js
+++ Website/consumption/consumption.js
@@ -160,7 +160,7 @@ function updateVoltageChart(data) {
                         ticks: {
                             beginAtZero: true,
                             max: 15, // Increased from 5 to 10 to accommodate higher voltage values
-                            stepSize: 1, // Changed from 0.5 to 1 for better readability
+                            stepSize: 1.5, // Changed from 0.5 to 1 for better readability
                             callback: function(value) {
                                 return value + ' V';
                             }
